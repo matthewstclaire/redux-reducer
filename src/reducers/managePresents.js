3a3e4b68c5ef2400b3ecf394af79bf3d23ec2929
matthewstclaire <matthew.crnich@gmail.com>
2021-03-09 14:28:35 -0700

    Done.

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,20 @@
-export function managePresents(state, action){
+export function managePresents(state, action) {
+  state = { numberOfPresents: 0 };
+  switch (action.type) {
+    case "INCREASE":
+      return { numberOfPresents: state.numberOfPresents + 1 };
+    default:
+      return state;
+  }
 }
+
+// function reducer(state, action) {
+//   switch (action.type) {
+//     case "INCREASE_COUNT":
+//       return { count: state.count + 1 };
+//     case "DECREASE_COUNT":
+//       return { count: state.count - 1 };
+//     default:
+//       return state;
+//   }
+// }
